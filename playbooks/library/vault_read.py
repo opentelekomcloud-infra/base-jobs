485737735991338f58108e9caa38867a7e1f6a63
Artem Goncharov <Artem.goncharov@gmail.com>
2022-01-11 14:42:20 +0100

    Update infra-prod job (#12)
    Update infra-prod job
    
    update pipeline detection
    update vault_read module
    
    Reviewed-by: None <None>
    Reviewed-by: OpenTelekomCloud Bot <None>

--- playbooks/library/vault_read.py
+++ playbooks/library/vault_read.py
@@ -9,14 +9,15 @@ from ansible.module_utils.urls import fetch_url
 class VaultReadModule():
     argument_spec = dict(
         vault_addr=dict(type='str', required=True),
-        role_id=dict(type='str', required=True),
-        secret_id=dict(type='str', no_log=True, required=True),
+        token=dict(type='str', required=False, no_log=True),
+        role_id=dict(type='str', required=False),
+        secret_id=dict(type='str', no_log=True, required=False),
         secret_name=dict(type='str', required=True),
-        mode=dict(type='str', choices=['cloud_config', 'plain'],
-                  default='plain')
     )
     module_kwargs = {
-        'supports_check_mode': True
+        'supports_check_mode': True,
+        'required_together': [('role_id', 'secret_id')],
+        'required_one_of': [('token', 'role_id')]
     }
 
     def __init__(self):
@@ -28,32 +29,50 @@ class VaultReadModule():
         self.module_name = self.ansible._name
         self.exit_json = self.ansible.exit_json
         self.fail_json = self.ansible.fail_json
+        self.vault_addr = None
+        self.token = None
 
-    def get_vault_token(self, vault_url, role_id, secret_id):
-        url = f"{vault_url}/v1/auth/approle/login"
-
-        data = self.ansible.jsonify(dict(
-            role_id=role_id,
-            secret_id=secret_id
-        ))
+    def _fetch(self, url, method, **kwargs):
         response, info = fetch_url(
             module=self.ansible,
             url=url,
-            method="POST",
-            data=data
+            method=method,
+            **kwargs
         )
         status = info['status']
 
         if status >= 400 and status != 404:
             self.fail_json(
-                msg='Failed to login to Vault',
+                msg=f'Failed to fetch {url}',
                 status_code=status
             )
         content = ""
         if response:
             content = response.read()
+        return (content, status)
+
+    def _get_secret_data(self, secret_name):
+        response, info = self._fetch(
+            f"{self.vault_addr}/v1/secret/data/{secret_name}",
+            "GET",
+            headers={
+                'X-Vault-Token': self.token
+            }
+        )
+        return json.loads(response)['data']['data']
+
+    def get_vault_token(self, role_id, secret_id):
+        url = f"{self.vault_addr}/v1/auth/approle/login"
+
+        data = self.ansible.jsonify(dict(
+            role_id=role_id,
+            secret_id=secret_id
+        ))
+        response, into = self._fetch(
+            url, "POST", data=data
+        )
         try:
-            body = json.loads(content)
+            body = json.loads(response)
             token = body['auth']['client_token']
             return token
         except Exception as ex:
@@ -63,58 +82,17 @@ class VaultReadModule():
             )
 
     def __call__(self):
-        vault_addr = self.params['vault_addr']
-        role_id = self.params['role_id']
-        secret_id = self.params['secret_id']
+        self.vault_addr = self.params['vault_addr']
         secret_name = self.params['secret_name']
-        mode = self.params['mode']
         result = {}
 
-        vault_token = self.get_vault_token(
-            vault_addr,
-            role_id,
-            secret_id
-        )
-        response, info = fetch_url(
-            module=self.ansible,
-            url=f"{vault_addr}/v1/secret/data/{secret_name}",
-            method="GET",
-            headers={
-                'X-Vault-Token': vault_token
-            }
-        )
-        status = info['status']
-        if status >= 400:
-            self.fail_json(
-                msg='Failed to fetch data from vault',
-                status_code=status,
-                info=info
-            )
-        content = ""
-        if response:
-            content = response.read()
-        try:
-            data = json.loads(content)
-            secret = data['data']['data']
-            cloud_config = dict(auth=dict())
-            if mode == 'plain':
-                result = secret
-            elif mode == 'cloud_config':
-                auth_attrs = [
-                    'auth_url', 'user_domain_name', 'username',
-                    'password', 'project_name', 'project_id']
-                for k, v in secret.items():
-                    if k in auth_attrs:
-                        cloud_config['auth'][k] = v
-                    else:
-                        cloud_config[k] = v
-                result = cloud_config
+        if self.params['role_id'] and self.params['secret_id']:
+            self.token = self.get_vault_token(
+                self.params['role_id'], self.params['secret_id'])
+        elif self.params['token']:
+            self.token = self.params['token']
 
-        except Exception as ex:
-            self.fail_json(
-                msg="Failed to process vault response",
-                error=str(ex)
-            )
+        result = self._get_secret_data(secret_name)
 
         self.exit_json(
             changed=False,
